f5476f955d33c7eda3407c4d8c0268b375e8f685
makhilbabu <manamakhilbabu@gmail.com>
2018-03-02 02:15:30 +0000

    Part2 check 16

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        new_array = Array.new
-        self.select("rating").uniq.each {|x| new_array.push(x.rating)}
+        new_array = self.uniq.pluck(:rating)
         new_array.sort.uniq()
     end
 end
