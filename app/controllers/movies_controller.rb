b65da67f76a496564c4cfa0265491e474ee85481
makhilbabu <manamakhilbabu@gmail.com>
2018-03-02 06:39:44 +0000

    Part 3 check 24

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,14 @@ class MoviesController < ApplicationController
   def index
     if (session[:sort_by].nil? and params[:sort_by].nil? and params[:ratings].nil? and session[:ratings].nil?)
       @movies = Movie.all
+      
+      @sort_column = params[:sort_by]
+    
+      @set_ratings = params[:set_ratings]
+    
+      if !@set_ratings
+        @set_ratings = Hash.new
+      end
       return
     end
     
