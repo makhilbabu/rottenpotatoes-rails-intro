22acc5183b3a814f79edecd356095646e820aca9
makhilbabu <manamakhilbabu@gmail.com>
2018-03-02 02:35:12 +0000

    Part2 check 19

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,17 +14,7 @@ class MoviesController < ApplicationController
     @movies = Movie.order(params[:sort_by])
     
     if params[:ratings]
-      if params[:sort_by]
-        @movies = Movie.where(:rating => params[:ratings].keys()).find(:all, :order=>(params[:sort_by]))
-      else
-        if params[:ratings]
-          @movies = Movie.where(:rating => params[:ratings].keys())
-        else
-          @movies = Movie
-        end
-      end
-    else
-      @movies = Movie.all
+      @movies = Movie.where(:rating => params[:ratings].keys()).find(:all, :order=>(params[:sort_by]))
     end
     
     @sort_column = params[:sort_by]
