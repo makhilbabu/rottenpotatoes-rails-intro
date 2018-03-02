54b0507d879fd87c6ad55590196231882642d4a4
makhilbabu <manamakhilbabu@gmail.com>
2018-03-02 00:02:48 +0000

    Part2 check_7

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        new_array = Model.uniq.pluck(:rating)
+        new_array = self.uniq.pluck(:rating)
         new_array.sort.uniq()
     end
 end
